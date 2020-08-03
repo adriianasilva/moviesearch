bd439ef5c19c51e65b58c705a56a77191c56ab5e
Adriana Silva <a.s.santana@hotmail.com>
2020-08-03 17:51:21 -0300

    Alterações no css da pag

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -5,16 +5,18 @@ const Card = () => {
     <>
             <div class="card">
             <div class="cardimage">
-                <img src="/home/adriana/projetos/moviesearch/moviesearch/src/components/Card/postertest.jpg" alt='cartaz'></img>
-            </div>
+            <img src="/home/adriana/projetos/moviesearch/moviesearch/src/components/Card/postertest.jpg" alt="cartaz" width="90" height="120"></img>
+          </div>
+           
             <div class="cardcontent">
-                <div id="titulo"><h1>Mussum Ipsum</h1></div>
+                <div class="titulo"><h1 id="titulo">Mussum Ipsum</h1></div>
                 <div class="box">75%</div>
                 <div id="data"><h3>27/04/1997</h3></div>
                 <div id="description">Mussum Ipsum, cacilds vidis litro abertis. Copo furadis é disculpa de bebadis, arcu quam euismod magna. Quem manda na minha terra sou euzis! Si num tem leite então bota uma pinga aí cumpadi! Nec orci ornare consequat. Praesent lacinia ultrices consectetur. Sed non ipsum felis. 
                 </div>
                 <div id="tags">tags</div>
-           
+                
+
             </div>
             </div> 
              
